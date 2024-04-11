63945ad65835790794491f60ce50fb8948962c5e
Eleonora Trikidou <trikidou@csd.auth.gr>
2024-04-11 18:06:09 +0300

    Add files via upload

---
+++ Edabit_Very_Easy.py
@@ -0,0 +1,110 @@
+##def hello():
+##    return "hello edabit.com"
+##
+##print(hello())
+
+
+##def addition(a, b):
+##    return a+b
+##
+##print(addition(3, 2))
+##print(addition(-3, -6))
+##print(addition(7, 3))
+
+
+##def addition(num):
+##    num=num+1
+##    return num
+##
+##print(addition(0))
+##print(addition(9))
+##print(addition(-3))
+
+
+##def convert(minutes):
+##    return minutes*60
+##
+##print(convert(5))
+##print(convert(3))
+##print(convert(2))
+
+
+##def tri_area(base, height):
+##    return (base * height) / 2
+##
+##print(tri_area(3, 2))
+##print(tri_area(7, 4))
+##print(tri_area(10, 10))
+
+
+##def cubes(a):
+##    return a ** 3
+##
+##print(cubes(3))
+##print(cubes(5))
+##print(cubes(10))
+
+
+##def how_many_seconds(hours):
+##    return hours*3600
+##
+##print(how_many_seconds(2))
+##print(how_many_seconds(10))
+##print(how_many_seconds(24))
+
+
+##def next_edge(side1, side2):
+##    return (side1+side2)-1
+##
+##print(next_edge(8, 10))
+##print(next_edge(5, 7))
+##print(next_edge(9, 2))
+
+
+##def remainder(x, y):
+##	return x%y
+##
+##print(remainder(1, 3))
+##print(remainder(3, 4))
+##print(remainder(5, 5))
+##print(remainder(7, 2))
+
+
+##def string_int(txt):
+##    return (int)(txt)
+##
+##print(string_int("6"))
+##print(string_int("1000"))
+##print(string_int("12"))
+
+
+##def calc_age(age):
+##    return age*365
+##
+##print(calc_age(65))
+##print(calc_age(0))
+##print(calc_age(20))
+
+
+##def find_perimeter(length, width):
+##    return 2*(length + width)
+##
+##print(find_perimeter(6, 7))
+##print(find_perimeter(20, 10))
+##print(find_perimeter(2, 9))
+
+
+##def circuit_power(voltage, current):
+##    return voltage*current
+##
+##print(circuit_power(230, 10))
+##print(circuit_power(110, 3))
+##print(circuit_power(480, 20))
+
+
+def sum_polygon(n):
+    return (n - 2) * 180
+
+print(sum_polygon(3))
+print(sum_polygon(4))
+print(sum_polygon(6))
